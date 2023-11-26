16c64f177ab18ad3d5539b5156ad5516a94ef107
Maruf Hossain <hossainmaruf001@gmail.com>
2023-11-26 17:00:32 +0100

    adoption request upon clicking on button implemented

--- src/pages/PetDetails/PetDetails.jsx
+++ src/pages/PetDetails/PetDetails.jsx
@@ -1,7 +1,52 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const PetDetails = () => {
+  const axiosSecure = useAxiosSecure();
   const pet = useLoaderData();
+  const { user } = useContext(AuthContext);
+  const handleSubmit = async (e) => {
+    const reqUserName = e.target?.name?.value;
+    const requestEmail = e.target?.email?.value;
+    const requestPhone = e.target?.phone?.value;
+    const requestAddress = e.target?.address?.value;
+    const petOwner = pet?.petOwner;
+    const petId = pet?._id;
+    if (requestEmail == petOwner) {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Your cant request for your own pet",
+        showConfirmButton: false,
+        timer: 2500,
+      });
+      return;
+    }
+    const reqInfo = {
+      reqUserName: reqUserName,
+      requestEmail: requestEmail,
+      requestPhone: requestPhone,
+      requestAddress: requestAddress,
+      petOwner: petOwner,
+      petId: petId,
+    };
+    console.log(reqInfo);
+    const petRes = await axiosSecure.post("/request/pets", reqInfo);
+    console.log(petRes.data);
+    if (petRes.data.insertedId) {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Pet request has been made",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
   console.log(pet);
   return (
     <div className="max-w-7xl mx-auto">
@@ -33,7 +78,106 @@ const PetDetails = () => {
             Description: <span>{pet.description}</span>
           </p>
           <div className="card-actions justify-center  m-4">
-            <button className="btn btn-primary">Adopt Pet</button>
+            {/* <button className="btn btn-primary">Adopt Pet</button> */}
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+            <button
+              className="btn btn-secondary"
+              onClick={() => document.getElementById("my_modal_5").showModal()}
+            >
+              Request for Adopt
+            </button>
+            <dialog
+              id="my_modal_5"
+              className="modal modal-bottom sm:modal-middle"
+            >
+              <div className="modal-box">
+                <h3 className="font-bold text-lg">Hello!</h3>
+                <p className="py-4">
+                  Press ESC key or click the button below to close
+                </p>
+                <div className="flex flex-col items-center justify-center">
+                  <form onSubmit={handleSubmit} method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <div>
+                      <label
+                        // for="first_name"
+                        className="block mb-2 text-sm font-medium text-pink-600  dark:text-white"
+                      >
+                        User Name
+                      </label>
+                      <input
+                        className=" text-2xl mr-2 bg-gray-400 text-white p-2 rounded-lg "
+                        type="text"
+                        name="name"
+                        value={user.displayName}
+                        readOnly
+                        id=""
+                        required
+
+                        // value={getDate()}
+                      />
+                    </div>
+                    <div>
+                      <label
+                        // for="first_name"
+                        className="block mb-2 text-sm font-medium text-pink-600  dark:text-white"
+                      >
+                        User email
+                      </label>
+                      <input
+                        className=" text-2xl mr-2 bg-gray-400 text-white p-2 rounded-lg "
+                        type="email"
+                        name="email"
+                        value={user.email}
+                        readOnly
+                        id=""
+                        required
+                        // value={getDate()}
+                      />
+                    </div>
+                    <div>
+                      <label
+                        // for="first_name"
+                        className="block mb-2 text-sm font-medium text-pink-600  dark:text-white"
+                      >
+                        Address
+                      </label>
+                      <input
+                        className=" text-2xl mr-2 bg-secondary text-white p-2 rounded-lg "
+                        type="text"
+                        name="address"
+                        id=""
+                        required
+                        // value={getDate()}
+                      />
+                    </div>
+                    <div>
+                      <label
+                        // for="first_name"
+                        className="block mb-2 text-sm font-medium text-pink-600  dark:text-white"
+                      >
+                        Phone Number
+                      </label>
+                      <input
+                        className=" text-2xl mr-2 bg-secondary text-white p-2 rounded-lg "
+                        type="number"
+                        name="phone"
+                        id=""
+                        required
+                        // value={getDate()}
+                      />
+                    </div>
+                    {/* <button className="btn">Close</button> */}
+                    <input
+                      className="btn bg-primary justify-center mx-auto flex "
+                      type="submit"
+                      value="Submit"
+                    />
+                    {/* <input type="submit" value="" /> */}
+                  </form>
+                </div>
+              </div>
+            </dialog>
           </div>
         </div>
       </div>
