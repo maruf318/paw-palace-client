de989355bcbc6d335db4a2f1bcddbada401433d6
Maruf Hossain <hossainmaruf001@gmail.com>
2023-11-28 16:30:50 +0100

    added mui component footer

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,6 +1,15 @@
+import { Rating } from "@mui/material";
+
 const Footer = () => {
   return (
     <footer className="footer footer-center p-4 bg-secondary text-base-content">
+      <p>According to user response</p>
+      <Rating
+        name="half-rating-read"
+        defaultValue={4.5}
+        precision={0.5}
+        readOnly
+      />
       <aside>
         <p>
           Copyright © 2023 - All right reserved by Maruf Hossain (creator of
